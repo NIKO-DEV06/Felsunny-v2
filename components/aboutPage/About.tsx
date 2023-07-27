55a64d9157785fa32c64c28b1fd85bdd69ec3a49
Ayeniko Emmanuel <ayenikoemmanuel06@gmail.com>
2023-07-27 14:58:14 +0100

    added about page and contact page animations

--- components/aboutPage/About.tsx
+++ components/aboutPage/About.tsx
@@ -6,6 +6,7 @@ import inspiration from "@/assets/inspiration.svg";
 import value from "@/assets/value.svg";
 import ceo from "@/assets/ceo.jpg";
 import ceo2 from "@/assets/ceo2.jpg";
+import { ImageReveal } from "../utils/ImageReveal";
 
 const About = () => {
   return (
@@ -23,100 +24,119 @@ const About = () => {
         <div>
           <div className="px-[1.5rem] md:px-[4rem] mt-[3rem] md:mt-[6rem] flex flex-col md:flex-row">
             <div className="pb-[2rem] md:w-1/2">
-              <Image
-                src={history}
-                alt="spin"
-                width={60}
-                height={60}
-                className="animate-spin-slow"
-              />
+              <ImageReveal>
+                <Image
+                  src={history}
+                  alt="spin"
+                  width={60}
+                  height={60}
+                  className="animate-spin-slow"
+                />
+              </ImageReveal>
             </div>
             <div className="flex flex-col gap-[4rem] md:w-[50%]">
               <div className="flex flex-col gap-[1rem]">
-                <p className="uppercase font-sofia tracking-widest text-[1.1rem] md:text-[0.8rem] md:w-[30rem] underline">
-                  Our History
-                </p>
-
-                <p className="font-sofia-regular text-[1.1rem] md:text-[1.2rem] md:font-sofia">
-                  Founded in 2010 by Felix Sunday Ayeniko in Nigeria, Felsunny
-                  Technology has grown from a vision into a powerhouse in the
-                  business solutions industry. We are dedicated to empowering
-                  businesses to reach new heights through innovative and
-                  tailored solutions, catering to a diverse range of industries
-                  and needs.
-                </p>
+                <Reveal y={80}>
+                  <p className="uppercase font-sofia tracking-widest text-[1.1rem] md:text-[0.8rem] md:w-[30rem] underline">
+                    Our History
+                  </p>
+                </Reveal>
+                <Reveal y={250}>
+                  <p className="font-sofia-regular text-[1.1rem] md:text-[1.2rem] md:font-sofia">
+                    Founded in 2010 by Felix Sunday Ayeniko in Nigeria, Felsunny
+                    Technology has grown from a vision into a powerhouse in the
+                    business solutions industry. We are dedicated to empowering
+                    businesses to reach new heights through innovative and
+                    tailored solutions, catering to a diverse range of
+                    industries and needs.
+                  </p>
+                </Reveal>
               </div>
             </div>
           </div>
 
           <div className="px-[1.5rem] md:px-[4rem] mt-[3rem] md:mt-[6rem] flex flex-col md:flex-row-reverse md:justify-between">
             <div className="pb-[2rem]">
-              <Image
-                src={inspiration}
-                alt="spin"
-                width={60}
-                height={60}
-                className="animate-spin-slow"
-              />
+              <ImageReveal>
+                <Image
+                  src={inspiration}
+                  alt="spin"
+                  width={60}
+                  height={60}
+                  className="animate-spin-slow"
+                />
+              </ImageReveal>
             </div>
             <div className="flex flex-col gap-[4rem] md:w-[50%]">
               <div className="flex flex-col gap-[1rem]">
-                <p className="uppercase font-sofia tracking-widest text-[1.1rem] md:text-[0.8rem] md:w-[30rem] underline">
-                  Inspiration
-                </p>
+                <Reveal y={80}>
+                  <p className="uppercase font-sofia tracking-widest text-[1.1rem] md:text-[0.8rem] md:w-[30rem] underline">
+                    Inspiration
+                  </p>
+                </Reveal>
 
-                <p className="font-sofia-regular text-[1.1rem] md:text-[1.2rem] md:font-sofia">
-                  From the outset, our founder, Felix Sunday Ayeniko, envisioned
-                  a company that would bridge the gap between businesses and
-                  success. With a deep understanding of the challenges faced by
-                  enterprises, Felsunny Technology set out to provide top-notch
-                  services that drive growth and prosperity.
-                </p>
+                <Reveal y={250}>
+                  <p className="font-sofia-regular text-[1.1rem] md:text-[1.2rem] md:font-sofia">
+                    From the outset, our founder, Felix Sunday Ayeniko,
+                    envisioned a company that would bridge the gap between
+                    businesses and success. With a deep understanding of the
+                    challenges faced by enterprises, Felsunny Technology set out
+                    to provide top-notch services that drive growth and
+                    prosperity.
+                  </p>
+                </Reveal>
               </div>
             </div>
           </div>
 
           <div className="px-[1.5rem] md:px-[4rem] mt-[3rem] md:mt-[6rem] flex flex-col md:flex-row">
             <div className="pb-[2rem] md:w-1/2">
-              <Image
-                src={value}
-                alt="spin"
-                width={60}
-                height={60}
-                className="animate-spin-slow invert"
-              />
+              <ImageReveal>
+                <Image
+                  src={value}
+                  alt="spin"
+                  width={60}
+                  height={60}
+                  className="animate-spin-slow invert"
+                />
+              </ImageReveal>
             </div>
             <div className="flex flex-col gap-[4rem] md:w-[50%]">
               <div className="flex flex-col gap-[1rem]">
-                <p className="uppercase font-sofia tracking-widest text-[1.1rem] md:text-[0.8rem] md:w-[30rem] underline">
-                  Our Values
-                </p>
-
-                <p className="font-sofia-regular text-[1.1rem] md:text-[1.2rem] md:font-sofia">
-                  At Felsunny, we put our clients at the heart of everything we
-                  do. By taking the time to understand your unique needs, we
-                  ensure our solutions align perfectly with your goals. Our
-                  client-focused approach has fostered strong, enduring
-                  partnerships that span various sectors and regions. Integrity,
-                  innovation, and reliability are not just words for us; they
-                  are the pillars of our work. As we continue to evolve and
-                  adapt to the ever-changing business landscape, our values
-                  remain steadfast.
-                </p>
+                <Reveal y={80}>
+                  <p className="uppercase font-sofia tracking-widest text-[1.1rem] md:text-[0.8rem] md:w-[30rem] underline">
+                    Our Values
+                  </p>
+                </Reveal>
+                <Reveal y={250}>
+                  <p className="font-sofia-regular text-[1.1rem] md:text-[1.2rem] md:font-sofia">
+                    At Felsunny, we put our clients at the heart of everything
+                    we do. By taking the time to understand your unique needs,
+                    we ensure our solutions align perfectly with your goals. Our
+                    client-focused approach has fostered strong, enduring
+                    partnerships that span various sectors and regions.
+                    Integrity, innovation, and reliability are not just words
+                    for us; they are the pillars of our work. As we continue to
+                    evolve and adapt to the ever-changing business landscape,
+                    our values remain steadfast.
+                  </p>
+                </Reveal>
               </div>
             </div>
           </div>
         </div>
         {/* /// */}
         <div className="absolute translate-x-1/2 right-1/2 w-[20rem] md:w-[25rem] md:h-[25rem] lg:w-[27rem] lg:h-[27rem] h-[20rem] roundedfull mx-auto mt-[4rem] lg:mt-[10rem] lg:scale-[1.3]">
-          <video
-            src="/ball.mp4"
-            autoPlay
-            loop
-            muted
-            playsInline
-            className="rounded-full"
-          ></video>
+          <ImageReveal>
+            <video
+              src="/ball.mp4"
+              autoPlay
+              loop
+              muted
+              playsInline
+              className="rounded-full"
+            ></video>
+          </ImageReveal>
         </div>
         {/* /// */}
 
@@ -127,17 +147,21 @@ const About = () => {
         </Reveal>
         <div className="flex flex-col justify-center items-center">
           <div className="overflow-hidden rounded-full w-auto h-auto">
-            <Image
-              src={ceo}
-              width={280}
-              height={280}
-              alt="ceo"
-              className="grayscale hover:grayscale-0 hover:scale-[1.1] duration-300 cursor-pointer rounded-full lg:w-[350px] lg:h-[350px]"
-            />
+            <ImageReveal>
+              <Image
+                src={ceo}
+                width={280}
+                height={280}
+                alt="ceo"
+                className="grayscale hover:grayscale-0 hover:scale-[1.1] duration-300 cursor-pointer rounded-full lg:w-[350px] lg:h-[350px]"
+              />
+            </ImageReveal>
           </div>
-          <p className="font-integral mt-[1.3rem] text-[1.35rem] italic md:text-[2rem] md:mt-[2rem]">
-            Felix Sunday Ayeniko
-          </p>
+          <Reveal y={100}>
+            <p className="font-integral mt-[1.3rem] text-[1.35rem] italic md:text-[2rem] md:mt-[2rem]">
+              Felix Sunday Ayeniko
+            </p>
+          </Reveal>
         </div>
       </div>
     </section>
--- components/contactPage/Contact.tsx
+++ components/contactPage/Contact.tsx
@@ -7,6 +7,8 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import emailjs from "@emailjs/browser";
 import * as yup from "yup";
 import toast, { Toaster } from "react-hot-toast";
+import { Reveal } from "../utils/Reveal";
+import { ImageReveal } from "../utils/ImageReveal";
 
 const Contact = () => {
   const [selectedServices, setSelectedServices] = useState<string[]>([]);
@@ -80,46 +82,52 @@ const Contact = () => {
       <Toaster position="top-center" reverseOrder={false} />
       <section className="min-h-screen bg-white text-black pb-[8rem]">
         <UnfixedHeader invert="" color="text-black" />
-        <h1 className="font-sofia-regular mt-[4.5rem] md:mt-[7rem] mb-[2rem] md:mb-[5rem] text-[3rem] md:text-[4.7rem] md:leading-[4.8rem] px-[2rem] md:px-[9rem] leading-[3.3rem]">
-          Hey! Tell us all
-          <br /> the things{" "}
-          <motion.span
-            animate={{ rotate: [0, 10, -8, 0] }}
-            transition={{
-              duration: 1.5,
-              repeat: Infinity,
-              repeatType: "reverse",
-              ease: "linear",
-            }}
-            className="inline-block"
-          >
-            👋
-          </motion.span>
-        </h1>
-        <p className="font-sofia-regular md:font-sofia px-[2rem] md:px-[9rem] text-[1.7rem] md:text-[2.3rem] opacity-30 mb-[2rem]">
-          I'm interested in...
-        </p>
+        <Reveal y={100}>
+          <h1 className="font-sofia-regular mt-[4.5rem] md:mt-[7rem] mb-[2rem] md:mb-[5rem] text-[3rem] md:text-[4.7rem] md:leading-[4.8rem] px-[2rem] md:px-[9rem] leading-[3.3rem]">
+            Hey! Tell us all
+            <br /> the things{" "}
+            <motion.span
+              animate={{ rotate: [0, 10, -8, 0] }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "reverse",
+                ease: "linear",
+              }}
+              className="inline-block"
+            >
+              👋
+            </motion.span>
+          </h1>
+        </Reveal>
+        <Reveal y={80}>
+          <p className="font-sofia-regular md:font-sofia px-[2rem] md:px-[9rem] text-[1.7rem] md:text-[2.3rem] opacity-30 mb-[2rem]">
+            I'm interested in...
+          </p>
+        </Reveal>
         <div className="px-[2rem] md:px-[9rem] flex flex-wrap gap-[0.7rem] md:gap-[1rem]">
           {services.map((service, index) => (
-            <button
-              key={index}
-              className={`border-[1px] border-[#00000045] w-fit px-[1.3rem] md:px-[1.8rem] py-[0.8rem] md:py-[1.2rem] font-sofia-regular md:font-sofia rounded-full text-[0.9rem] md:text-[1.2rem] group ${
-                selectedServices.includes(service)
-                  ? "bg-black text-white transition duration-[250ms] ease-in-out"
-                  : ""
-              }`}
-              onClick={() => handleServiceClick(service)}
-            >
-              <div className="w-fit h-[1.8rem] md:h-[2rem] overflow-hidden flex flex-col gap-[0.3rem]">
-                <p className="translate-y-[.2rem] md:translate-y[-1rem] md:group-hover:translate-y-[-2rem] duration-[30ms] cubic">
-                  {service}
-                </p>
-                <p className="md:group-hover:translate-y-[-1.8rem] hidden md:block duration-[300ms] cubic">
-                  {service}
-                </p>
-              </div>
-              <div />
-            </button>
+            <Reveal y={50}>
+              <button
+                key={index}
+                className={`border-[1px] border-[#00000045] w-fit px-[1.3rem] md:px-[1.8rem] py-[0.8rem] md:py-[1.2rem] font-sofia-regular md:font-sofia rounded-full text-[0.9rem] md:text-[1.2rem] group ${
+                  selectedServices.includes(service)
+                    ? "bg-black text-white transition duration-[250ms] ease-in-out"
+                    : ""
+                }`}
+                onClick={() => handleServiceClick(service)}
+              >
+                <div className="w-fit h-[1.8rem] md:h-[2rem] overflow-hidden flex flex-col gap-[0.3rem]">
+                  <p className="translate-y-[.2rem] md:translate-y[-1rem] md:group-hover:translate-y-[-2rem] duration-[30ms] cubic">
+                    {service}
+                  </p>
+                  <p className="md:group-hover:translate-y-[-1.8rem] hidden md:block duration-[300ms] cubic">
+                    {service}
+                  </p>
+                </div>
+                <div />
+              </button>
+            </Reveal>
           ))}
         </div>
 
@@ -128,73 +136,82 @@ const Contact = () => {
           onSubmit={handleSubmit(submitFormHandler)}
           className="px-[2rem] md:px-[9rem] mt-[4rem] md:pt-[5rem] flex flex-col gap-[3rem] md:gap-[5rem]"
         >
-          <div>
-            <input
-              type="text"
-              {...register("name")}
-              name="name"
-              autoComplete="off"
-              placeholder="Your name"
-              className={`border-b-[0.9px] md:border-b-[1.5px] ${
-                errors.name
-                  ? "border-b-[#ff0000] focus:border-b-[#ff0000]"
-                  : "border-b-[#00000045] focus:border-b-black"
-              } w-full rounded-none appearance-none outline-none pb-[0.6rem] font-sofia-regular text-[1.7rem] placeholder:opacity-50 md:text-[2.5rem] md:font-sofia relative`}
-            />
-            <p className="text-[#ff0000] font-sofia md:text-[1.1rem] text-left pt-1 md:pt-[1rem] absolute tracking-wide">
-              {errors.name?.message}
-            </p>
-          </div>
-          <div>
-            <input
-              type="text"
-              {...register("email")}
-              name="email"
-              autoComplete="off"
-              placeholder="Your email"
-              className={`border-b-[0.9px] md:border-b-[1.5px] ${
-                errors.email
-                  ? "border-b-[#ff0000] focus:border-b-[#ff0000]"
-                  : "border-b-[#00000045] focus:border-b-black"
-              } w-full rounded-none appearance-none outline-none pb-[0.6rem] font-sofia-regular text-[1.7rem] placeholder:opacity-50 md:text-[2.5rem] md:font-sofia relative`}
-            />
-            <p className="text-[#ff0000] font-sofia md:text-[1.1rem] text-left pt-1 md:pt-[1rem] absolute tracking-wide">
-              {errors.email?.message}
-            </p>
-          </div>
-          <div>
-            <input
-              type="text"
-              {...register("message")}
-              name="message"
-              autoComplete="off"
-              placeholder="Tell us about your project"
-              className={`border-b-[0.9px] md:border-b-[1.5px] ${
-                errors.message
-                  ? "border-b-[#ff0000] focus:border-b-[#ff0000]"
-                  : "border-b-[#00000045] focus:border-b-black"
-              } w-full rounded-none appearance-none outline-none pb-[0.6rem] font-sofia-regular text-[1.7rem] placeholder:opacity-50 md:text-[2.5rem] md:font-sofia relative`}
-            />
-            <p className="text-[#ff0000] font-sofia md:text-[1.1rem] text-left pt-1 md:pt-[1rem] absolute tracking-wide">
-              {errors.message?.message}
-            </p>
-          </div>
+          <Reveal y={100} width="100%">
+            <div>
+              <input
+                type="text"
+                {...register("name")}
+                name="name"
+                autoComplete="off"
+                placeholder="Your name"
+                className={`border-b-[0.9px] md:border-b-[1.5px] ${
+                  errors.name
+                    ? "border-b-[#ff0000] focus:border-b-[#ff0000]"
+                    : "border-b-[#00000045] focus:border-b-black"
+                } w-full rounded-none appearance-none outline-none pb-[0.6rem] font-sofia-regular text-[1.7rem] placeholder:opacity-50 md:text-[2.5rem] md:font-sofia relative`}
+              />
+              <p className="text-[#ff0000] font-sofia md:text-[1.1rem] text-left pt-1 md:pt-[1rem] absolute tracking-wide">
+                {errors.name?.message}
+              </p>
+            </div>
+          </Reveal>
+          <Reveal y={100} width="100%">
+            <div>
+              <input
+                type="text"
+                {...register("email")}
+                name="email"
+                autoComplete="off"
+                placeholder="Your email"
+                className={`border-b-[0.9px] md:border-b-[1.5px] ${
+                  errors.email
+                    ? "border-b-[#ff0000] focus:border-b-[#ff0000]"
+                    : "border-b-[#00000045] focus:border-b-black"
+                } w-full rounded-none appearance-none outline-none pb-[0.6rem] font-sofia-regular text-[1.7rem] placeholder:opacity-50 md:text-[2.5rem] md:font-sofia relative`}
+              />
+              <p className="text-[#ff0000] font-sofia md:text-[1.1rem] text-left pt-1 md:pt-[1rem] absolute tracking-wide">
+                {errors.email?.message}
+              </p>
+            </div>
+          </Reveal>
+          <Reveal y={100} width="100%">
+            <div>
+              <input
+                type="text"
+                {...register("message")}
+                name="message"
+                autoComplete="off"
+                placeholder="Tell us about your project"
+                className={`border-b-[0.9px] md:border-b-[1.5px] ${
+                  errors.message
+                    ? "border-b-[#ff0000] focus:border-b-[#ff0000]"
+                    : "border-b-[#00000045] focus:border-b-black"
+                } w-full rounded-none appearance-none outline-none pb-[0.6rem] font-sofia-regular text-[1.7rem] placeholder:opacity-50 md:text-[2.5rem] md:font-sofia relative`}
+              />
+              <p className="text-[#ff0000] font-sofia md:text-[1.1rem] text-left pt-1 md:pt-[1rem] absolute tracking-wide">
+                {errors.message?.message}
+              </p>
+            </div>
+          </Reveal>
+
           <input
             type="hidden"
             name="selectedServices"
             value={selectedServicesString}
           />
-          <button
-            disabled={isSending}
-            className={`font-sofia-regular text-[1.5rem] border-b-2 w-fit border-black md:border-[#00000045] ${
-              isSending && "text-[#00000045]"
-            } mt-[1rem] md:text-[1.7rem] relative group`}
-          >
-            {isSending ? "Sending..." : "Send Request"}
-            <div
-              className={`scale-x-0 hidden md:block md:absolute w-full h-[2px] bg-black bottom-[-2px] group-hover:scale-x-100 duration-300`}
-            ></div>
-          </button>
+          <Reveal y={100} width="100%">
+            <button
+              disabled={isSending}
+              className={`font-sofia-regular text-[1.5rem] border-b-2 w-fit border-black md:border-[#00000045] ${
+                isSending && "text-[#00000045]"
+              } mt-[1rem] md:text-[1.7rem] relative group`}
+            >
+              {isSending ? "Sending..." : "Send Request"}
+              <div
+                className={`scale-x-0 hidden md:block md:absolute w-full h-[2px] bg-black bottom-[-2px] group-hover:scale-x-100 duration-300`}
+              ></div>
+            </button>
+          </Reveal>
         </form>
       </section>
     </>
